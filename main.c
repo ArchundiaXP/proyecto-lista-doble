9f93ba97b47703b3633a31cf125c49d38e5e22c0
Carlos Ramos <ArchundiaXP@outlook.com>
2023-11-28 23:36:53 -0600

    creacion de struct nodo

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
+/*
+Autor: Archundia
+programa: sistema de control de horarios de transporte usand lista doble
+fecha: 28-11-2023
+descripcion: el programa es capas de:
+
+*/
+
+/*bibliotecas*/
 #include <stdio.h>
 #include <stdlib.h>
+
+typedef struct chofer{
+    nombreChofer[20];
+    apellidoP[20];
+    apellidoM[20];
+}
+
 
 int main(){
 
