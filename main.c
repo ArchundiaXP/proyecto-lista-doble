f25ef5c772eda1b450a3f4ba7b6e147af97b1e92
Carlos Ramos <ArchundiaXP@outlook.com>
2023-12-01 18:50:05 -0600

    se crearon funciones crear inicio, crear final

--- main.c
+++ main.c
@@ -11,11 +11,12 @@ descripcion: el programa es capas de:
 
 
 typedef struct nodo{
-    int numNodo;
+    int idViaje[10];
     char hrRegistro[20];
     char nombreChofer[20];
     char apellidoP[20];
     char apellidoM[20];
+
     int numUnidad;
     char lugarSalida[50];
     char lugarLlegada[50];
@@ -41,6 +42,8 @@ void registraDatos(pNodo *P){
     printf("Registro de control de horarios - %s \n");
 
     printf("Ingrese los datos solicitados...\n");
+    printf("ID del viaje: ");
+    scanf("%d", &(*P)->idViaje);
     printf("Nombre de conductor:");
     scanf("%s", &(*P)->nombreChofer);
     printf("Apellido Paterno: ");
@@ -64,12 +67,84 @@ void registraDatos(pNodo *P){
 }
 
 /*
+nombre: crearInicio
+fecha: 17-11-2023
+objetivo: inicializar la lista
 */
 void crearInicio (pNodo *P){
+    pNodo Q;
+    char opc;
+    system("cls"); /*limpia la pantalla*/
+
+    /*generamos nuevo nodo*/
+    *P =(pNodo)malloc(sizeof(tNodo));
+    registraDatos(P);
+
+    (*P)->siguiente = NULL;
+    (*P)->anterior = NULL;
+
+    do{
+            Q = (pNodo) malloc (sizeof(tNodo));
+            registraDatos(&Q);
+
+            Q->siguiente = *P;
+            Q->anterior = NULL;
+
+            (*P)-> siguiente = Q;
+            *P = Q;
+
+        printf("Desea agregar otro registro? (Y/N): ");
+        scanf("%c", &opc);
+
+    }while (opc == 'Y' || opc == 'y');
+    
+    printf("tarea (CI) completada correctamente\n");
+}
+
+/*
+nombre: crearFinal
+fecha: 17-11-2023
+objetivo: generar registros al final de la lista
+
+*/
+void crearFinal (pNodo *P){
+    pNodo Q, T;
+    char opc;
+
+    system("cls");
+    *P = (pNodo) malloc (sizeof(tNodo));
+    registraDatos(P);
+
+    (*P)->siguiente = NULL;
+    (*P)->anterior = NULL;
+    T = *P;
+
+    do{
+        Q= (pNodo) malloc(sizeof(tNodo));
+        registraDatos(&Q);
+
+        Q->siguiente = NULL;
+        Q->anterior = *P;
+        T=Q;
+
+        printf("Desea agregar otro registro? (Y/N): ");
+        scanf("%c", &opc);
+
+    }while (opc == 'Y' || opc == 'y');
+
+    printf("tarea (CF) completada correctamente\n");
 
 }
 
+/*
+nombre:
+fecha: 01/12/2023
+objetivo: 
+*/
+void recorrerIterativo ()
+
 int main(){
+
 
     return 0;
 }
